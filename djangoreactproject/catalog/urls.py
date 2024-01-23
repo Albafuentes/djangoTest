f058de2e8f4eef86efe04f0a45fed2f275a131e4
alba <alba.fuentes@m47labs.ai>
2024-01-23 13:35:58 +0100

    create login page

--- djangoreactproject/catalog/urls.py
+++ djangoreactproject/catalog/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
-
+from django.urls import path, include
+from django.contrib.auth.views import LoginView
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("form/", views.form, name="form"),
+    path("login/", views.login, name='login'),
 ]
--- djangoreactproject/catalog/views.py
+++ djangoreactproject/catalog/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
+from django.contrib.auth import get_user_model
 
 # Create your views here.
 def index(request):
     title = "title de prueba"
     return HttpResponse(title)
 
-def form(request):
-    title = "title de prueba"
-    context = { "title": title }
-    return render(request, 'form.html', context=context)
+def login(request):
+    User = get_user_model()
+    user = User(email="normal@user.com", password="foo")
+    user_form = User(email=request.POST.get('username'), password=request.POST.get('password'))
+
+    if user.password == user_form.password:
+        request.session['username'] = user_form.username
+        session = "has logged"
+    else:
+        session = "has not logged"
+
+    context = {"session": session}
+    return render(request, 'login.html', context=context)
